a16361196bd9a0350144ae3023f82f4598949ba1
nitz-prnv <nitprnv@gmail.com>
2021-04-20 14:06:37 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React,{useEffect, useState} from 'react';
-import  {TextField} from '@material-ui/core';
+import  {TextField,Button} from '@material-ui/core';
 //importing Anime component
 import Anime from './contaainer/Anime';
 import './App.css';
@@ -17,6 +17,7 @@ useEffect(()=>{
 const getAnime = async ()=>{
   const response= await fetch(`https://api.jikan.moe/v3/search/anime?q=${query}&order_by=members&genre=0&genre_exclude=12`);
   const data = await response.json();
+  console.log(data)
   setlist(data.results);
   
 }
@@ -34,18 +35,18 @@ const getQuery = e=>{
 
 return(
   <div className='App'>
-      <form onSubmit={getQuery}>
-      <TextField value={search} onChange={UpdateSearch}/>
-      <button type='submit'>
+      <form onSubmit={getQuery} style={{marginTop:"5px"}}>
+      <TextField variant="standard" value={search} color="secondary" onChange={UpdateSearch}/>
+      <Button type='submit' variant="outlined" color="secondary">
        search
-      </button>
+      </Button>
       </form>
 
   <div className='container'>
   { 
     Lists.map(
     list =>(
-   <Anime key={list.mal_id} title={list.title} image={list.image_url} details={list.synopsis}/>
+   <Anime key={list.mal_id} title={list.title} image={list.image_url} details={list.synopsis} episode={list.episodes} rating={list.score} more={list.url}/>
     )
   )}
   </div>
--- src/contaainer/Anime.jsx
+++ src/contaainer/Anime.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import {Card, Typography} from '@material-ui/core'
 import './Anime.css'
-const Anime =({title,image,details})=>{
+const Anime =({title,image,details,episode,rating,more})=>{
     
     return(
         <div classname='container'>
         <Card className='card'>
           <h1>{title}</h1>  
           <img src={image} alt=''/> 
-          <Typography>{details}</Typography>
+          <Typography>{details}
+          <div>{""}</div>
+            <div> <b>Number of Episodes :</b>&nbsp;{episode}</div>
+            <div> <b> Ratings :</b>&nbsp;{rating}🌟</div>
+            for more information <a href={more}> click here</a>
+            </Typography>
+          
       </Card>
     </div>
 
